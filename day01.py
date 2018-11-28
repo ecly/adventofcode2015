79970850cfcecda5cb068046809e2e64dbe6f706
ecly <ecly@mailbox.org>
2018-11-29 00:04:28 +0100

    second half day02

--- day01.py
+++ day01.py
@@ -1,3 +1,13 @@
 with open("input", "r") as f:
     directions = f.read()
     print(directions.count("(") - directions.count(")"))
+
+    pos = 0
+    for idx, c in enumerate(directions):
+        if c == "(":
+            pos += 1
+        if c == ")":
+            pos -= 1
+        if pos < 0:
+            print(idx+1)
+            break
